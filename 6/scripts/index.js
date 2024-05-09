897a82a0685bc1336353f6d1eb324abacf10e897
Kelluin <Kelluin@users.noreply.github.com>
2024-05-09 17:37:37 +0000

    Deploying to gh-pages from @ Kelluin/244136-cat-energy-31@7a80d375ce307d67a711212f37ad7250c12454f3 🚀

---
+++ 6/scripts/index.js
@@ -0,0 +1,1 @@
+document.querySelector(".nav-button").addEventListener("click",()=>{document.querySelector(".site-nav__list").classList.toggle("site-nav__list--opened"),document.querySelector(".nav-button").classList.toggle("nav-button--opened")});
